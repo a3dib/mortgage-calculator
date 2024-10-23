1fd4e040a05c6852b632517a5c75c9261d0c51cd
a3dib <adibpuc@gmail.com>
2024-10-24 00:27:46 +0600

    0.2

--- src/components/Sliderselect.js
+++ src/components/Sliderselect.js
@@ -4,7 +4,13 @@ import Slidercomponent from './common/Slidercomponent'
 const Sliderselect = () => {
   return (
     <div>
-      <Slidercomponent/>
+      <Slidercomponent amount={3500} unit="$" step={100} defaultValue={4000} min={1000} max={10000} label="Home Value" onChange={(e) => console.log(e.target.value)}/>
+      
+      <Slidercomponent amount={600} unit="$" step={100} defaultValue={600} min={0} max={3000} label="Down Payment" onChange={(e) => console.log(e.target.value)}/>
+
+      <Slidercomponent amount={2000} unit="$" step={100} defaultValue={2000} min={0} max={3000} label="Loan Amount" onChange={(e) => console.log(e.target.value)}/>
+
+      <Slidercomponent amount={5} unit="%" step={1} defaultValue={5} min={2} max={18} label="Interest Rate" onChange={(e) => console.log(e.target.value)}/>
     </div>
   )
 }
--- src/components/common/Slidercomponent.js
+++ src/components/common/Slidercomponent.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import Slider from '@mui/material/Slider';
+import { Stack, Step, Typography } from '@mui/material';
 
-const Slidercomponent = () => {
+const Slidercomponent = ({ defaultValue, min, max, amount, label, unit, step, value, onChange}) => {
   return (
-        <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+    <Stack my={2}>
+      <Stack gap={1}>
+        <Typography variant="subtitle1">{label}</Typography>
+        <Typography variant='h4'>{unit}{amount}</Typography>
+      </Stack>
+      <Slider min={min} max={max} defaultValue={defaultValue} aria-label="Default" valueLabelDisplay="auto" step={step} marks value={value} onChange={onChange}/>
+      <Stack direction='row' justifyContent='space-between' >
+        <Typography variant='caption' color='text.secondary'>{unit} {min}</Typography>
+        <Typography color='text.secondary'>{unit} {max}</Typography>
+      </Stack>
+    </Stack>
   )
 }
 
