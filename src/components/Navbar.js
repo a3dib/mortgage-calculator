7faf992753658499a838ee05701a6c0a5d7e4782
a3dib <adibpuc@gmail.com>
2024-10-23 20:43:03 +0600

    update 2.2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,19 @@ import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
 
 
 const Navbar = () => {
   return (
-        <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-            Mortgage Calculator
-          </Typography>
-        </Toolbar>
+      <AppBar position="static">
+        <Container maxWidth>
+          <Toolbar>
+            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+              Mortgage Calculator
+            </Typography>
+          </Toolbar>
+        </Container>
       </AppBar>
   )
 }
